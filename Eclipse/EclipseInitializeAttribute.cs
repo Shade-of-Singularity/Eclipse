57d1e93b386376ca2083aa7e4ad397d40098bba1
DarkJune <56516330+DarkJune0122@users.noreply.github.com>
2026-01-09 06:39:41 +0900

    Added new initialization attributes
    They will be used during Engine initialization, to reduce mods overrelying on Engine.OnInitialized and similar events.

---
+++ Eclipse/EclipseInitializeAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Eclipse
+{
+    /// <summary>
+    /// Forces static class to run its static .ctor with <see cref="Eclipse"/>.<see cref="Engine"/>. 
+    /// </summary>
+    /// <remarks>
+    /// Static .ctor will run after <see cref="EngineService"/>s were instantiated, but BEFORE they were initialized.
+    /// Read this as - it will be dangerous to interact with <see cref="EngineService{T}.Instance"/>s at this point.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class EclipseInitializeAttribute : Attribute
+    {
+        /// <summary>
+        /// When attribute should be employed.
+        /// </summary>
+        public readonly InitializationTiming Timing;
+
+
+
+
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+        /// .
+        /// .                                                Constructors
+        /// .
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+        /// <summary>
+        /// Default constructor. Runs method in the latest possible point during engine initialization.
+        /// <para>See also: <see cref="InitializationTiming.AfterEngineInitialization"/>.</para>
+        /// </summary>
+        public EclipseInitializeAttribute() : this(InitializationTiming.AfterEngineInitialization) { }
+
+        /// <summary>
+        /// Full constructor. Allows you to specify when exactly attribute will used.
+        /// </summary>
+        /// <param name="timing"></param>
+        public EclipseInitializeAttribute(InitializationTiming timing)
+        {
+            Timing = timing;
+        }
+    }
+}
---
+++ Eclipse/EclipseUnloadingAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Eclipse
+{
+    /// <summary>
+    /// Runs methods with this attribute when <see cref="Eclipse"/>.<see cref="Engine"/> unloads from the memory.
+    /// </summary>
+    /// <remarks>
+    /// Unless manually requested to be unloaded, unloads only on <see cref="UnityEngine.Application.quitting"/>.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class EclipseUnloadingAttribute : Attribute
+    {
+        /// <summary>
+        /// When attribute should be employed.
+        /// </summary>
+        public readonly UnloadingTiming Timing;
+
+
+
+
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+        /// .
+        /// .                                                Constructors
+        /// .
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+        /// <summary>
+        /// Default constructor. Runs method in the latest possible point during engine unloading.
+        /// <para>See also: <see cref="UnloadingTiming.AfterEngineUnloading"/>.</para>
+        /// </summary>
+        public EclipseUnloadingAttribute() : this(UnloadingTiming.AfterEngineUnloading) { }
+
+        /// <summary>
+        /// Full constructor. Allows you to specify when exactly attribute will used.
+        /// </summary>
+        /// <param name="timing"></param>
+        public EclipseUnloadingAttribute(UnloadingTiming timing)
+        {
+            Timing = timing;
+        }
+    }
+}
